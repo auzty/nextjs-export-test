6e1fa0875cc9a12a2d26e24d070900908265b31f
yusuf <yusuf>
2022-01-07 10:08:46 +0700

    next-config locale copy

---
+++ src/nonsupportedfiles/next.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  async rewrites(){
+    return [
+      {
+        source: '/about-us', 
+        destination: '/sample/rewrites/about',
+      },
+      {
+        source: '/sample/rewrites/external',
+        destination: 'https://www.google.com',
+      },
+  ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/sample/redirects/google',
+        destination: 'https://www.google.com',
+        permanent: true,
+      },
+      {
+        source: '/sample/redirects/root',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/sample/headers',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'ini headernya customlho',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'yss lambada',
+          },
+        ],
+      },
+    ]
+  },
+}
